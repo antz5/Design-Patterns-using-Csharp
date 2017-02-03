c60f4e8d15334ec99bc91418e83610ea2aadbf88
Ananth <antz5@users.noreply.github.com>
2017-02-03 16:55:43 +1100

    Singleton Design pattern

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Design-Patterns-using-Csharp
 Design pattern Code snippets using C#
+
+What are design patterns?
+According to wiki(https://en.wikipedia.org/wiki/Software_design_pattern), 
+
+1. In software engineering, a software design pattern is a general reusable solution to a commonly occurring problem within a given context in software design.
+2. It is not a finished design that can be transformed directly into source or machine code.
+3. It is a description or template for how to solve a problem that can be used in many different situations.
+4. Design patterns are formalized best practices that the programmer can use to solve common problems when designing an application or system.
+5. Object-oriented design patterns typically show relationships and interactions between classes or objects, without specifying the final application classes or objects that are involved.
+6. Patterns that imply mutable state may be unsuited for functional programming languages, some patterns can be rendered unnecessary in languages that have built-in support for solving the problem they are trying to solve, and object-oriented patterns are not necessarily suitable for non-object-oriented languages.
+7. Design patterns may be viewed as a structured approach to computer programming intermediate between the levels of a programming paradigm and a concrete algorithm.
+
+
+Singleton falls under creational design patterns category, which ensures a class has only one instance and provide a global point of access to it.
---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var instance = Singleton.GetInstance();
+            var instance2 = Singleton.GetInstance();
+            if(instance.Equals(instance2))
+                Console.WriteLine("instance.Equals(instance2) is equal to "+ instance.Equals(instance2));
+            else
+                Console.WriteLine("instance.Equals(instance2) is equal to " + instance.Equals(instance2));
+
+            if(instance==instance2)
+                Console.WriteLine("instance == instance2 is equal to "+(instance==instance2));
+            else
+                Console.WriteLine("instance == instance2 is equal to " + (instance == instance2));
+            Console.ReadKey();
+        }
+    }
+
+    //Lazy initialization
+    public class Singleton
+    {
+        private static Singleton _instance;
+
+        private Singleton()
+        { }
+
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+
+            return _instance;
+        }
+    }
+
+    //Simple Thread safety
+    public class SingletonThreadSafe
+    {
+        private static SingletonThreadSafe _instance;
+        private static readonly object lockObj = new object();
+        private SingletonThreadSafe() { }
+
+        public static SingletonThreadSafe GetInstance()
+        {
+            lock (lockObj)
+            {
+                if (_instance == null)
+                {
+                    _instance = new SingletonThreadSafe();
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    //Not Lazy but thread safe without using lock
+    public class SingletonThreadSafeWithoutLock
+    {
+        private static readonly SingletonThreadSafeWithoutLock _instance = new SingletonThreadSafeWithoutLock();
+
+        static SingletonThreadSafeWithoutLock() { }
+        private SingletonThreadSafeWithoutLock() { }
+
+        public static SingletonThreadSafeWithoutLock GetInstance()
+        {
+            return _instance;
+        }
+    }
+}
